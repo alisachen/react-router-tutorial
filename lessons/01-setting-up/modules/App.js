c2a803e6b795c92a8f9e4f36972972da1c009bc6
Alisa <chao.chen4@hp.com>
2017-04-24 23:13:52 +0800

    Learn react-router

--- lessons/01-setting-up/modules/App.js
+++ lessons/01-setting-up/modules/App.js
@@ -1,7 +1,21 @@
 import React from 'react'
+import {IndexLink} from 'react-router'
+import NavLink from './NavLink'
+import Home from './Home'
 
 export default React.createClass({
   render() {
-    return <div>Hello, React Router!</div>
+    return (
+      <div>
+        <h1>React Router tutorial</h1>
+        <ul role="nav">
+          <li><NavLink to="/about">About12</NavLink></li>
+          <li><NavLink to="/repos">Repos12</NavLink></li>
+          <li><IndexLink to="/" activeClassName="active">Home</IndexLink></li>
+        </ul>
+        
+        {this.props.children}
+      </div>
+    )
   }
 })
